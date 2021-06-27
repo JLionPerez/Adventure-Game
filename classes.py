84fcf09a920072e872620c60b3a025f5bbf45c75
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2021-06-26 20:31:21 -0700

    finished Character and Profession classes

--- classes.py
+++ classes.py
@@ -1,47 +1,40 @@
 class Character:
-    # default variables
     name = ""
     profession = ""
     health = 0
     spells = {
-        "Light Heal" : ["heal", 5],
-        "Burn" : ["attack", 2],
-        "Frost" : ["attack", 3]
+        "" : ["heal", 0],
+        "" : ["attack", 0]
     }
 
-    # constructor
     def __init__(self, Profession, name):
+        player_spells = dict(Profession.spells)
         self.name = name
-        self.profession = Profession.profession_title
-    
-    # passive functions
+        self.profession = Profession.title
+        self.health = Profession.health
+        self.spells = player_spells
+
     def add_health(self, bonus_health):
         self.health = self.health + bonus_health
+
+    def damage(self, damage_received):
+        self.health = self.health - damage_received
     
     def new_spell(self, new_spell, type, points):
         self.spells[new_spell] = [type, points]
-
-    #  action functions
-    def damage(self, damage_received):
-        self.health = self.health - damage_received
+    
+    def forget_spell(self, spell):
+        del self.spells[spell]
 
 class Profession:
-    # default variables
-    profession_title = ""
+    title = ""
     health = 0
     spells = {
-        "Light Heal" : ["heal", 5],
-        "Burn" : ["attack", 2],
-        "Frost" : ["attack", 3]
+        "Potion" : ["heal", 5],
+        "Punch" : ["attack", 2]
     }
 
-    def __init__(self, title, health):
-        self.profession_title = title
+    def __init__(self, title, health, default_spells):
+        self.title = title
         self.health = health
-
-    def set_profession(self, profession):
-        self.profession_title = profession
-
-ob1 = Profession("Thief", 30)
-ob2 = Character(ob1, "Mary")
-print (ob2.profession)
+        self.spells = default_spells
